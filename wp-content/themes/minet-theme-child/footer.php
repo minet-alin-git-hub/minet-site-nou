0f08ef57c4d747d52805ae7966fb81579216198d
minet-alin-git <alin.sofianu@minet.ro>
2025-10-29 09:38:34 +0200

    add before morning 10/29/2025

--- wp-content/themes/minet-theme-child/footer.php
+++ wp-content/themes/minet-theme-child/footer.php
@@ -1,13 +1,53 @@
-
-		<footer>
-			<div class="container h-align">
-				<?php if(!empty(get_field('logo', 'options'))) echo '<a href="/">'.wp_get_attachment_image(get_field('logo', 'options')['ID'], '', '', array('class'=>'logo', 'alt'=>'minet-logo')).'</a>'; ?>
-				<?php if(!empty(get_field('copy', 'options'))) echo '<p class="copy">© MINET '.get_the_date('Y').'. '.get_field('copy', 'options').'</p>'; ?>
+<footer>
+	<div class="container">	
+		<div class="footer-columns">
+			<div class="footer-left">
+				<?php 
+				if(!empty(get_field('logo', 'options'))) 
+					echo '<a href="/">'.wp_get_attachment_image(get_field('logo', 'options')['ID'], '', '', array('class'=>'logo', 'alt'=>'minet-logo')).'</a>'; 
+				?>
+				<div class="footer-socials">
+					<a href="https://www.linkedin.com" target="_blank">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/linkedin.svg" alt="LinkedIn">
+					</a>
+					<a href="https://www.xing.com" target="_blank">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/xing.svg" alt="Xing">
+					</a>
+					<a href="https://www.youtube.com" target="_blank">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/youtube.svg" alt="youtube">
+					</a>
+					<a href="https://www.instagram.com" target="_blank">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/instagram.svg" alt="Instagram">
+					</a>
+					<a href="https://www.facebook.com" target="_blank">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/facebook.svg" alt="facebook">
+					</a>
+				</div>
 			</div>
-		</footer>
-		<?php get_template_part('template-parts/general/general', 'svg'); ?>
-		<?php wp_footer(); ?>
-		<!-- if dns prefetch include third-party code here -->
-
-	</body>
+			<div class="footer-center">
+				<p>MINET S.A.<br>
+				Strada Depozitelor, nr. 12<br>
+				Râmnicu Vâlcea,<br>
+				România<br><br>
+				TELEFON:<br>
+				0350 401 710<br>
+				office@minet.ro</p>
+			</div>
+			<div class="footer-right">
+				<ul>
+					<li><a href="#">Terms & Conditions</a></li>
+					<li><a href="#">Imprint</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li><a href="#">Downloads</a></li>
+					<li><a href="#">Press</a></li>
+					<br>
+					<li><a href="#">Privacy-Settings</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+<?php get_template_part('template-parts/general/general', 'svg'); ?>
+<?php wp_footer(); ?>
+</body>
 </html>
--- wp-content/themes/minet-theme-child/functions.php
+++ wp-content/themes/minet-theme-child/functions.php
@@ -2,30 +2,13 @@
 function child_enqueue_styles() {
     // Load parent theme styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style('child-style', get_stylesheet_uri(), ['parent-style']);
 
-    // Industrial / Rugged font options (with 800 weight):
-
-    // 1. Oswald – strong, condensed sans-serif
     wp_enqueue_style( 'font-oswald', 'https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700;800&display=swap', false );
-
-    // 2. Anton – heavy, bold industrial headline (only one weight available, behaves like 800)
     wp_enqueue_style( 'font-anton', 'https://fonts.googleapis.com/css2?family=Anton&display=swap', false );
-
-    // 3. Roboto Slab – geometric serif, professional
     wp_enqueue_style( 'font-roboto-slab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700;800&display=swap', false );
-
-    // 4. Archivo – modern, versatile industrial body text
     wp_enqueue_style( 'font-archivo', 'https://fonts.googleapis.com/css2?family=Archivo:wght@400;600;700;800&display=swap', false );
-
-    // Monospace / technical fonts:
-
-    // 5. Share Tech Mono – mechanical monospace (single weight)
     wp_enqueue_style( 'font-share-tech-mono', 'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap', false );
-
-    // 6. IBM Plex Mono – professional technical monospace
     wp_enqueue_style( 'font-ibm-plex-mono', 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600;700;800&display=swap', false );
-
-    // 7. Fira Code – developer favorite monospace
-    wp_enqueue_style( 'font-fira-code', 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700;800&display=swap', false );
-}
+    wp_enqueue_style( 'font-fira-code', 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700;800&display=swap', false );}
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
