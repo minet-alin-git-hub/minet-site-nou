26287f99b44db03d98a1ec3ab9ddafd56b396489
minet-alin-git <alin.sofianu@minet.ro>
2025-10-30 09:41:51 +0200

    morning 10/30/2025

--- wp-content/themes/minet-theme-child/functions.php
+++ wp-content/themes/minet-theme-child/functions.php
@@ -1,14 +1,25 @@
 <?php
 function child_enqueue_styles() {
-    // Load parent theme styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style('child-style', get_stylesheet_uri(), ['parent-style']);
-
-    wp_enqueue_style( 'font-oswald', 'https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700;800&display=swap', false );
-    wp_enqueue_style( 'font-anton', 'https://fonts.googleapis.com/css2?family=Anton&display=swap', false );
-    wp_enqueue_style( 'font-roboto-slab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700;800&display=swap', false );
-    wp_enqueue_style( 'font-archivo', 'https://fonts.googleapis.com/css2?family=Archivo:wght@400;600;700;800&display=swap', false );
-    wp_enqueue_style( 'font-share-tech-mono', 'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap', false );
-    wp_enqueue_style( 'font-ibm-plex-mono', 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600;700;800&display=swap', false );
-    wp_enqueue_style( 'font-fira-code', 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700;800&display=swap', false );}
+}
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
+
+function minet_child_register_tehnologie() {
+    $labels = array(
+        'name' => 'Tehnologii',
+        'singular_name' => 'Tehnologie',
+        'add_new_item' => 'Add New Tehnologie',
+        'edit_item' => 'Edit Tehnologie',
+        'all_items' => 'All Tehnologii'
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array('title','editor','thumbnail','excerpt'),
+        'show_in_rest' => false
+    );
+    register_post_type('tehnologie', $args);
+}
+add_action('init', 'minet_child_register_tehnologie');
