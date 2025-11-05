15c9c6b6604a195345ca47f4163c2e9b87072551
minet-alin-git <alin.sofianu@minet.ro>
2025-11-05 10:48:07 +0200

    add 05/11/2025

--- wp-content/themes/minet-theme-child/templates/technology.php
+++ wp-content/themes/minet-theme-child/templates/technology.php
@@ -12,12 +12,21 @@
 
   <?php if(get_field('image_screen_visibility') == true) get_template_part('template-parts/general/general', 'one-image-screen', get_field('image_screen_image')); ?>
 
-  <?php if(get_field('info_screen_visibility') == true)
-  $info_list = (object)[
-    'list'=>get_field('info_list'),
-  ];
-  get_template_part('template-parts/general/general', 'info-boxes', $info_list); 
-  ?>
+  <?php if(get_field('info_screen_visibility') == true):
+    $info_list = get_field('info_list');
+    if($info_list):
+      $formatted_list = [];
+      foreach($info_list as $item):
+        $formatted_list[] = [
+          'title' => $item['title'],
+          'info' => $item['info'],
+          'link' => $item['link_to_subpage'],
+        ];
+      endforeach;
+      $info_list_obj = (object)['list'=>$formatted_list];
+      get_template_part('template-parts/general/general', 'info-boxes', $info_list_obj);
+    endif;
+  endif; ?>
 
 </main>
 <?php get_footer(); ?>
