0626fdb6d8b8d849afc4289c922eee26f4fa064d
minet-alin-git <alin.sofianu@minet.ro>
2025-11-11 11:14:04 +0200

    add 11/11/2025

--- wp-content/themes/minet-theme-child/template-parts/general/general-info-boxes.php
+++ wp-content/themes/minet-theme-child/template-parts/general/general-info-boxes.php
@@ -1,25 +1,32 @@
-<?php if(!empty($args)): ?>
-<section class="info_boxes_screen">
-  <div class="container">
-    <?php if(!empty($args->title)): ?>
-      <div class="section__title left_title" data-inviewport="section-title"><svg width="74px" height="45px"><use xlink:href="#section-title"></use></svg><h2 class="section-title"><?php echo $args->title; ?></h2></div>
-    <?php endif; ?>
-    <ul class="info_boxes">
+<?php if (!empty($args)): ?>
+  <section class="info_boxes_screen">
+    <div class="container">
+      <?php if (!empty($args->title)): ?>
+        <div class="section__title left_title" data-inviewport="section-title"><svg width="74px" height="45px">
+            <use xlink:href="#section-title"></use>
+          </svg>
+          <h2 class="section-title"><?php echo $args->title; ?></h2>
+        </div>
+      <?php endif; ?>
+      <ul class="info_boxes">
         <?php foreach ($args->list as $key => $value): ?>
-            <li>
-            <h3 data-inViewport="changeColor" style="animation-delay:<?php echo $key*1000 / 2; ?>ms;" ><?php echo $value['title']; ?></h3>
-            <div class="info"><?php echo $value['info']; ?></div>
-            <?php if(!empty($value['link'])): ?>
+          <li>
+            <?php if (!empty($value['image'])) echo wp_get_attachment_image($value['image']['ID'], 'full'); ?>
+            <div class="info-content">
+              <h3 data-inViewport="changeColor" style="animation-delay:<?php echo $key * 1000 / 2; ?>ms;"><?php echo $value['title']; ?></h3>
+              <div class="info"><?php echo $value['info']; ?></div>
+              <?php if (!empty($value['link'])): ?>
                 <a href="<?php echo esc_url($value['link']['url']); ?>" target="<?php echo esc_attr($value['link']['target']); ?>">
-                <span><?php echo esc_html($value['link']['title']); ?></span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path d="M13 5l7 7-7 7M5 12h14" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
+                  <span><?php echo esc_html($value['link']['title']); ?></span>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M13 5l7 7-7 7M5 12h14" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
                 </a>
-            <?php endif; ?>
-            </li>
+              <?php endif; ?>
+            </div>
+          </li>
         <?php endforeach; ?>
-    </ul>
-  </div>
-</section>
+      </ul>
+    </div>
+  </section>
 <?php endif; ?>
--- wp-content/themes/minet-theme-child/templates/technology.php
+++ wp-content/themes/minet-theme-child/templates/technology.php
@@ -2,31 +2,43 @@
 <?php get_header(); ?>
 <main class="technology">
 
-  <?php if(!empty(get_the_content())): ?>
-  <section class="sc1">
-    <div class="center-info container h-align">
-      <div class="info"><?php the_content(); ?></div>
-    </div>
-  </section>
+  <?php if (!empty(get_the_content())): ?>
+    <section class="sc1">
+      <div class="center-info container h-align">
+        <div class="info"><?php the_content(); ?></div>
+      </div>
+    </section>
   <?php endif; ?>
 
-  <?php if(get_field('image_screen_visibility') == true) get_template_part('template-parts/general/general', 'one-image-screen', get_field('image_screen_image')); ?>
+  <?php if (get_field('image_screen_visibility') == true) get_template_part('template-parts/general/general', 'one-image-screen', get_field('image_screen_image')); ?>
 
-  <?php if(get_field('info_screen_visibility') == true):
+  <?php if (get_field('info_screen_visibility') == true):
     $info_list = get_field('info_list');
-    if($info_list):
+    if ($info_list):
       $formatted_list = [];
-      foreach($info_list as $item):
+      foreach ($info_list as $item):
         $formatted_list[] = [
           'title' => $item['title'],
           'info' => $item['info'],
           'link' => $item['link_to_subpage'],
+          'image' => $item['grid_boxes_img'],
         ];
       endforeach;
-      $info_list_obj = (object)['list'=>$formatted_list];
+      $info_list_obj = (object)['list' => $formatted_list];
       get_template_part('template-parts/general/general', 'info-boxes', $info_list_obj);
     endif;
   endif; ?>
+
+  <?php
+  $button_group = get_field('ti_button'); // your ACF group with first and second button
+  if ($button_group):
+    get_template_part('template-parts/general/general-technology-applications', null, (object)[
+      'title' => get_field('section_title'),
+      'info'  => get_field('section_info'),
+      'buttons' => $button_group
+    ]);
+  endif;
+  ?>
 
 </main>
 <?php get_footer(); ?>
